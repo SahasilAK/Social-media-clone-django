59a15c0d9381a2f88cb00dbcaa61dfb8b26214d5
Sahasil AK <76594303+SahasilAK@users.noreply.github.com>
2021-02-12 03:14:02 +0530

    added profile pic croping

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from account.views import (
                             account_view,
                             edit_account_view,
+                            crop_image,
                             # account_search_view,
 
                             )
@@ -10,9 +11,10 @@ from account.views import (
 app_name = 'account'
 
 urlpatterns = [
-      
+
       path('<user_id>/',account_view,name="view"),
       path('<user_id>/edit/',edit_account_view,name="edit"),
+      path('<user_id>/edit/cropImage',crop_image,name="crop_image"),
       # path('search/',account_search_view,name="search"),
 
 
