adf3ae8c92a00c6f5875bb7b77414cb33958f28e
Sahasil AK <76594303+SahasilAK@users.noreply.github.com>
2021-02-12 16:20:06 +0530

    added accept and decline friend requests

--- friend/views.py
+++ friend/views.py
@@ -1,8 +1,26 @@
 from django.http import HttpResponse
 import json
-
+from django.shortcuts import render, redirect
 from account.models import Account
-from friend.models import FriendRequest
+from friend.models import FriendRequest, FriendList
+
+
+
+def friend_requests(request, *args, **kwargs):
+	context = {}
+	user = request.user
+	if user.is_authenticated:
+		user_id = kwargs.get("user_id")
+		account = Account.objects.get(pk=user_id)
+		if account == user:
+			friend_requests = FriendRequest.objects.filter(receiver=account, is_active=True)
+			context['friend_requests'] = friend_requests
+
+		else:
+			return HttpResponse("You can't view another users friend requests.")
+	else:
+		return redirect('login')
+	return render(request, "friend/friend_requests.html", context)
 
 
 def send_friend_request(request, *args, **kwargs):
@@ -38,4 +56,79 @@ def send_friend_request(request, *args, **kwargs):
 			payload['response'] = "Unable to sent a friend request."
 	else:
 		payload['response'] = "You must be authenticated to send a friend request."
+	return HttpResponse(json.dumps(payload), content_type="application/json")
+
+
+def accept_friend_request(request, *args, **kwargs):
+	user = request.user
+	payload = {}
+
+	if request.method == 'GET' and user.is_authenticated:
+		friend_request_id = kwargs.get("friend_request_id")
+
+		if friend_request_id:
+			friend_request = FriendRequest.objects.get(pk=friend_request_id)
+			#confirm that i sthe correct request
+			if friend_request.receiver == user:
+				if friend_request:
+					#found the request . now accept it
+					updated_notification = friend_request.accept()
+					payload['response'] = "Friend request accepted."
+				else:
+					payload['response'] = "Something went wrong."
+			else:
+				payload['response'] = "That is not your request to accept."
+		else:
+			payload['response'] = "Unable to accept that friend request."
+	else:
+		# should never happen
+		payload['response'] = "You must be authenticated to accept a friend request."
+	return HttpResponse(json.dumps(payload), content_type="application/json")
+
+
+def remove_friend(request, *args, **kwargs):
+	user = request.user
+	payload = {}
+
+	if request.method == 'POST' and user.is_authenticated:
+		user_id = request.POST.get("receiver_user_id")
+		if user_id:
+			try:
+				removee = Account.objects.get(pk=user_id)
+				friend_list = FriendList.objects.get(user=user)
+				friend_list.unfriend(removee)
+
+				payload['response'] = "Successfully removed that friend."
+			except Exception as e:
+				payload['response'] = f"Something went wrong: {str(e)}"
+
+		else:
+			payload['response'] = "There was an error. Unable to remove that friend."
+	else:
+		# should never happen
+		payload['response'] = "You must be authenticated to remove a friend."
+	return HttpResponse(json.dumps(payload), content_type="application/json")
+
+def decline_friend_request(request, *args, **kwargs):
+	user = request.user
+	payload = {}
+	if request.method == "GET" and user.is_authenticated:
+		friend_request_id = kwargs.get("friend_request_id")
+		if friend_request_id:
+			friend_request = FriendRequest.objects.get(pk=friend_request_id)
+			# confirm that is the correct request
+			if friend_request.receiver == user:
+				if friend_request:
+					# found the request. Now decline it
+					updated_notification = friend_request.decline()
+					payload['response'] = "Friend request declined."
+				else:
+					payload['response'] = "Something went wrong."
+			else:
+				payload['response'] = "That is not your friend request to decline."
+		else:
+			payload['response'] = "Unable to decline that friend request."
+	else:
+		# should never happen
+		payload['response'] = "You must be authenticated to decline a friend request."
 	return HttpResponse(json.dumps(payload), content_type="application/json")
