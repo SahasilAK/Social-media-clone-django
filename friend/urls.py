16bc1636f521d68b3719bdc9dc50b9dd3fb364da
Sahasil AK <76594303+SahasilAK@users.noreply.github.com>
2021-02-12 16:42:23 +0530

    added cancel friend request

--- friend/urls.py
+++ friend/urls.py
@@ -4,6 +4,7 @@ from friend.views import (send_friend_request,
                                 accept_friend_request,
                                 decline_friend_request,
                                 remove_friend,
+                                cancel_friend_request,
                                 )
 
 
@@ -15,6 +16,7 @@ urlpatterns = [
    path('friend_request_accept/<friend_request_id>/', accept_friend_request, name='friend-request-accept'),
    path('friend_request_decline/<friend_request_id>/', decline_friend_request, name='friend-request-decline'),
    path('friend_remove/', remove_friend, name='remove-friend'),
+   path('friend_request_cancel/', cancel_friend_request, name='friend-request-cancel'),
 
 
 ]
