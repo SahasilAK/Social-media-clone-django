e3f4d27f2197bb72ecd74bdb4b6d1585e1a807b9
Sahasil AK <76594303+SahasilAK@users.noreply.github.com>
2021-02-12 16:34:16 +0530

    added remove friend function

--- friend/urls.py
+++ friend/urls.py
@@ -3,6 +3,7 @@ from friend.views import (send_friend_request,
                                 friend_requests,
                                 accept_friend_request,
                                 decline_friend_request,
+                                remove_friend,
                                 )
 
 
@@ -13,6 +14,7 @@ urlpatterns = [
    path('friend_requests/<user_id>/', friend_requests, name='friend-requests'),
    path('friend_request_accept/<friend_request_id>/', accept_friend_request, name='friend-request-accept'),
    path('friend_request_decline/<friend_request_id>/', decline_friend_request, name='friend-request-decline'),
-   
+   path('friend_remove/', remove_friend, name='remove-friend'),
+
 
 ]
